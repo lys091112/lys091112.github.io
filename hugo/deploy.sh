046aeca097760beff734ce9740a26ab1fe4b7be3
crescent <lys091112@gmail.com>
2022-07-08 22:35:06 +0800

    deploy

--- hugo/deploy.sh
+++ hugo/deploy.sh
@@ -4,12 +4,26 @@ hugo -t even
 echo "生成html文件"
 
 git checkout master
+if [ $? -ne 0]; then
+    echo "切换到master分支h失败，请检查！"
+    exit;
+fi
 
 path=`cd .. && pwd`
 echo "当前路径：$path"
+
 rm -rf $path/docs
 mv $path/hugo/public $path/docs
-git add .
-git commit -m "deploy"
-git push origin master
-echo "推送成功"
+
+git add . && git commit -m "deploy" && git push origin master
+
+if [ $? -ne 0]; then
+    echo "推送到master失败，请手动处理！"
+    exit;
+else
+    echo "推送成功"
+fi
+
+
+echo "切回hugo分支"
+git checkout hugo
