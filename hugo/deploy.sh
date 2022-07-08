0d2aa110f6cf7917defdc96f693f3c7b80f022f5
crescent <lys091112@gmail.com>
2022-07-08 22:48:20 +0800

    发布脚步更新

--- hugo/deploy.sh
+++ hugo/deploy.sh
@@ -15,18 +15,16 @@ fi
 path=`cd .. && pwd`
 echo "当前路径：$path"
 
-rm -rf $path/docs
-mv $path/hugo/public $path/docs
+rm -rf $path/docs &&  mv $path/hugo/public $path/docs
 
 git add . && git commit -m "deploy" && git push origin master
 
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
     echo "推送到master失败，请手动处理！"
     exit;
 else
     echo "推送成功"
 fi
-
 
 echo "切回hugo分支"
 git checkout hugo
